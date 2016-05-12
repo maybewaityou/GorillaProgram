dbfdd0b1019d3ee6f66f4915b339a60a5a5bc9fb
MeePwn <maybewaityou@gmail.com>
2016-05-12 10:20:38 +0800

    initialize

--- GorillaProgram/ViewController.swift
+++ GorillaProgram/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
