a12f8267409cba5f6fe87681deddfa1e44059942
MeePwn <maybewaityou@gmail.com>
2016-05-16 09:51:57 +0800

    add UIWindow+Extension

---
+++ GorillaProgram/Classes/Main/Extension/UIWindow+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIWindow+Extension.swift
+//  GorillaProgram
+//
+//  Created by ChunNan on 16/5/16.
+//  Copyright © 2016年 MeePwn. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+    
+    func switchRootController() {
+        
+    }
+    
+}
