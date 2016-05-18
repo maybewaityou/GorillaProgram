cf4b42c048e829b4faaa323c08c7db67fb0be7de
MeePwn <maybewaityou@gmail.com>
2016-05-18 17:48:28 +0800

    add SignalProducer

--- GorillaProgram/Classes/Home/ViewModel/HomeViewModel.swift
+++ GorillaProgram/Classes/Home/ViewModel/HomeViewModel.swift
@@ -27,45 +27,19 @@ class HomeViewModel: ViewModel<HomeDataModel> {
     }
     
     private func buttonCommandExecute() -> RACSignal {
-//        let (signal, observer) = Signal<String, NSError>.pipe()
-//
-//        signal.observeNext { (str) in
-//            print("=====>>> \(str)")
-//        }
-//        observer.sendNext("hahahah")
-        
-//        let producer = countToValue(5)
-//        producer.startWithNext { (index) in
-//            print("=====>>> \(index)")
-//        }
-//        producer.startWithSignal { (observer, disposable) in
-//            observer.observeNext({ (index) in
-//                print("== index ===>>> \(index)")
-//            })
-//            observer.observeCompleted({ 
-//                print("=====>>> success")
-//            })
-//        }
-        
-//        let producer = SignalProducer<Int, NSError> { (observer, disposable) in
-//            
-//        }
         service.getNetworkService()
             .producer(.GET, url: "https://api.github.com", params: ["": ""], model: User())
             .startWithNext { (user) in
                 print("=====>>> \(user)")
             }
-        
-        
+        return RACSignal.empty()
 //        let dataModel = HomeDataModel.init(type: .Home)
 //        service.pushWithDataModel(dataModel)
 //        return service.getNetworkService()
 //            .signalWithRequestMethid(.GET, url: "https://api.github.com", params: ["": ""], model: User())
 //            .doNextAs({ (model: User) in
-//
 //                print("=====>>> \(model.books)")
 //            })
-        return RACSignal.empty()
     }
     
 }
