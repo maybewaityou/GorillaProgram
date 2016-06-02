792b3d9428c3b6f3a4e191d258458ab936fef3ef
MeePwn <maybewaityou@gmail.com>
2016-06-02 10:12:39 +0800

    add UIBarButtonItem Extension

--- GorillaProgram/Classes/Framework/Extension/UIBarButtonItem+Extension.swift
+++ GorillaProgram/Classes/Framework/Extension/UIBarButtonItem+Extension.swift
@@ -11,6 +11,7 @@ import ReactiveCocoa
 
 extension UIBarButtonItem {
     
+    /*====================================== Native Method Start ======================================*/
     class func itemWithImage(image: String, onClickListener: ((AnyObject!) -> Void)!) -> UIBarButtonItem {
         let button = buttonByImage(image, highImage: image)
         button.rac_signalForControlEvents(.TouchUpInside).subscribeNext(onClickListener)
@@ -18,11 +19,18 @@ extension UIBarButtonItem {
         return UIBarButtonItem.init(customView: button)
     }
     
-    class func itemWithImage(image: String, highImage: String, command: RACCommand) -> UIBarButtonItem {
+    class func itemWithImage(image: String, highImage: String, onClickListener: ((AnyObject!) -> Void)!) -> UIBarButtonItem {
         let button = buttonByImage(image, highImage: highImage)
-        button.rac_command = command
+        button.rac_signalForControlEvents(.TouchUpInside).subscribeNext(onClickListener)
         
-        return UIBarButtonItem.init(customView: button);
+        return UIBarButtonItem.init(customView: button)
+    }
+    
+    class func itemWithImage(image: String, highImage: String, title: String, onClickListener: ((AnyObject!) -> Void)!) -> UIBarButtonItem {
+        let button = buttonByImage(image, highImage: highImage, title: title)
+        button.rac_signalForControlEvents(.TouchUpInside).subscribeNext(onClickListener)
+        
+        return UIBarButtonItem.init(customView: button)
     }
     
     class func itemWithTitle(title: String, onClickListener: ((AnyObject!) -> Void)!) -> UIBarButtonItem {
@@ -40,6 +48,22 @@ extension UIBarButtonItem {
         
         return UIBarButtonItem.init(customView: button)
     }
+    /*====================================== Native Method End ======================================*/
+    
+    /*====================================== RAC Method Start ======================================*/
+    class func itemWithImage(image: String, highImage: String, command: RACCommand) -> UIBarButtonItem {
+        let button = buttonByImage(image, highImage: highImage)
+        button.rac_command = command
+        
+        return UIBarButtonItem.init(customView: button);
+    }
+    
+    class func itemWithImage(image: String, highImage: String, title: String, command: RACCommand) -> UIBarButtonItem {
+        let button = buttonByImage(image, highImage: highImage, title: title)
+        button.rac_command = command
+        
+        return UIBarButtonItem.init(customView: button);
+    }
     
     class func itemWithTitle(title: String, command: RACCommand) -> UIBarButtonItem {
         return itemWithTitle(title, highTitle: title, command: command)
@@ -51,25 +75,43 @@ extension UIBarButtonItem {
         
         return UIBarButtonItem.init(customView: button)
     }
+    /*====================================== RAC Method End ======================================*/
     
+    /*====================================== Private Method Start ======================================*/
     private class func buttonByImage(image: String, highImage: String) -> UIButton {
         let button = UIButton.init(type: .Custom)
-        button.setBackgroundImage(UIImage.init(named: image), forState: .Normal)
-        button.setBackgroundImage(UIImage.init(named: highImage), forState: .Highlighted)
-        button.size = button.currentBackgroundImage!.size
+        button.setImage(UIImage.init(named: image), forState: .Normal)
+        button.setImage(UIImage.init(named: highImage), forState: .Highlighted)
+        button.sizeToFit()
         
         return button
     }
     
-    private class func buttonByTitle(title: String, highTitle: String) -> UIButton {
+    private class func buttonByImage(image: String, highImage: String, title: String, titleColor: UIColor = UIColor.white(), highTitleColor: UIColor = UIColor.white()) -> UIButton {
+        let button = UIButton.init(type: .Custom)
+        button.setImage(UIImage.init(named: image), forState: .Normal)
+        button.setImage(UIImage.init(named: highImage), forState: .Highlighted)
+        button.setTitle(title, forState: .Normal)
+        button.setTitle(title, forState: .Highlighted)
+        button.setTitleColor(titleColor, forState: .Normal)
+        button.setTitleColor(highTitleColor, forState: .Highlighted)
+        button.sizeToFit()
+        
+        button.contentEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 0)
+        
+        return button
+    }
+    
+    private class func buttonByTitle(title: String, highTitle: String, titleColor: UIColor = UIColor.white(), highTitleColor: UIColor = UIColor.white()) -> UIButton {
         let button = UIButton.init(type: .Custom)
         button.setTitle(title, forState: .Normal)
         button.setTitle(highTitle, forState: .Highlighted)
-        button.setTitleColor(UIColor.white(), forState: .Normal)
-        button.setTitleColor(UIColor.white(), forState: .Highlighted)
-        button.size = CGSizeMake(50, 44);
+        button.setTitleColor(titleColor, forState: .Normal)
+        button.setTitleColor(highTitleColor, forState: .Highlighted)
+        button.sizeToFit()
         
         return button
     }
+    /*====================================== Private Method End ======================================*/
     
 }
