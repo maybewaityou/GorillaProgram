4adef0c9c21268de44d65dd9a71c9dad8f6fe28d
MeePwn <maybewaityou@gmail.com>
2016-06-15 17:59:47 +0800

    make sendRequest method execute in background thread

--- GorillaProgram/Classes/Framework/Network/NetworkApi.swift
+++ GorillaProgram/Classes/Framework/Network/NetworkApi.swift
@@ -17,8 +17,8 @@ public enum RequestMethod: String {
 }
 
 class NetworkApi: NSObject {
-
-/*====================================== Common Method Start ======================================*/
+    
+    /*====================================== Common Method Start ======================================*/
     /** 配置证书 */
     class func configureCertificate() {
         allowAllCertificate()
@@ -29,20 +29,18 @@ class NetworkApi: NSObject {
         print("== url ===>>> \(url)")
         print("== params ===>>> \(params)")
         return Observable.create { (observer) -> RxSwift.Disposable in
-            Async.background(block: {
-                sendRequest(method, url: url, params: params, completionHandler: { (response) -> Void in
-                    let result = response.result.value!
-                    
-                    print("=====>>> \(result)")
-                    
-                    observer.onNext(ModelAdapter.model(result, model: model))
-                    observer.onCompleted()
-                })
+            sendRequest(method, url: url, params: params, completionHandler: { (response) -> Void in
+                let result = response.result.value!
+                
+                print("=====>>> \(result)")
+                
+                observer.onNext(ModelAdapter.model(result, model: model))
+                observer.onCompleted()
             })
             return NopDisposable.instance
-        }
-        .subscribeOn(MainScheduler.asyncInstance)
-        .observeOn(MainScheduler.instance)
+            }
+            .subscribeOn(MainScheduler.asyncInstance)
+            .observeOn(MainScheduler.instance)
     }
     
     /** 公共请求方法 - RAC 4+ */
@@ -50,19 +48,17 @@ class NetworkApi: NSObject {
         print("== url ===>>> \(url)")
         print("== params ===>>> \(params)")
         return SignalProducer<M, NSError> { (observer, disposable) -> () in
-            Async.background(block: {
-                sendRequest(method, url: url, params: params, completionHandler: { (response) -> Void in
-                    let result = response.result.value!
-                    
-                    print("=====>>> \(result)")
-                    
-                    observer.sendNext(ModelAdapter.model(result, model: model))
-                    observer.sendCompleted()
-                })
+            sendRequest(method, url: url, params: params, completionHandler: { (response) -> Void in
+                let result = response.result.value!
+                
+                print("=====>>> \(result)")
+                
+                observer.sendNext(ModelAdapter.model(result, model: model))
+                observer.sendCompleted()
             })
-        }
-        .startOn(QueueScheduler.init())
-        .observeOn(UIScheduler.init())
+            }
+            .startOn(QueueScheduler.init())
+            .observeOn(UIScheduler.init())
     }
     
     /** 公共请求方法 - RAC 3+ */
@@ -70,50 +66,50 @@ class NetworkApi: NSObject {
         print("== url ===>>> \(url)")
         print("== params ===>>> \(params)")
         return RACSignal.createSignal({ (subscriber) -> RACDisposable! in
-            Async.background(block: {
-                sendRequest(method, url: url, params: params, completionHandler: { (response) -> Void in
-                    let result = response.result.value!
-                    
-                    print("=====>>> \(result)")
-                    
-                    subscriber.sendNext(ModelAdapter.model(result, model: model))
-                    subscriber.sendCompleted()
-                })
+            sendRequest(method, url: url, params: params, completionHandler: { (response) -> Void in
+                let result = response.result.value!
+                
+                print("=====>>> \(result)")
+                
+                subscriber.sendNext(ModelAdapter.model(result, model: model))
+                subscriber.sendCompleted()
             })
             return RACDisposable.init(block: { () -> Void in
             })
         })
-        .subscribeOn(RACScheduler.immediateScheduler())
-        .deliverOn(RACScheduler.mainThreadScheduler())
+            .subscribeOn(RACScheduler.immediateScheduler())
+            .deliverOn(RACScheduler.mainThreadScheduler())
     }
-/*====================================== Common Method End ======================================*/
-
-/*====================================== Private Method Start ======================================*/
+    /*====================================== Common Method End ======================================*/
+    
+    /*====================================== Private Method Start ======================================*/
     // Get请求
     private class func getJsonWithRequest(url: String, completionHandler: Response<AnyObject, NSError> -> Void) {
         Alamofire.request(.GET, url).responseJSON(completionHandler: completionHandler);
     }
-
+    
     // Post请求
     private class func postJsonWithRequest(url: String, params: Dictionary<String, String>, completionHandler: Response<AnyObject, NSError> -> Void) {
         let headers = [
             "":"",
             "":"",
             "":"",
-        ]
+            ]
         Alamofire.request(.POST, url, parameters: params, encoding: .JSON, headers: headers).responseJSON(completionHandler: completionHandler)
     }
-
+    
     private class func sendRequest(method: RequestMethod, url: String, params: Dictionary<String, String>, completionHandler: Response<AnyObject, NSError> -> Void) {
-        if method == .GET {
-            getJsonWithRequest(url, completionHandler: completionHandler)
-        } else {
-            postJsonWithRequest(url, params: params, completionHandler: completionHandler)
-        }
+        Async.background(block: {
+            if method == .GET {
+                getJsonWithRequest(url, completionHandler: completionHandler)
+            } else {
+                postJsonWithRequest(url, params: params, completionHandler: completionHandler)
+            }
+        })
     }
-/*====================================== Private Method End ======================================*/
-
-/*====================================== Configure Certificate Start ======================================*/
+    /*====================================== Private Method End ======================================*/
+    
+    /*====================================== Configure Certificate Start ======================================*/
     /** 允许所有证书 */
     private class func allowAllCertificate() {
         let manager = Manager.sharedInstance
@@ -138,6 +134,6 @@ class NetworkApi: NSObject {
             return (disposition, credential)
         }
     }
-/*====================================== Configure Certificate Start ======================================*/
+    /*====================================== Configure Certificate Start ======================================*/
     
 }
